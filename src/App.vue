d114fa611f39bf75d6f503cceece527e9a413621
Abd Elmalik Abd Elghafar <paydaychains100@gmail.com>
2023-05-01 10:05:05 +0300

    fixes

--- src/App.vue
+++ src/App.vue
@@ -81,7 +81,7 @@ import { ref } from 'vue'
 const defaultNum = ref(1)
 const lightBox = ref(false)
 const images = [
-  '/src/assets/images/image-product-1-thumbnail.jpg',
+  '../public/images/image-product-1-thumbnail.jpg',
   '/src/assets/images/image-product-2-thumbnail.jpg',
   '/src/assets/images/image-product-3-thumbnail.jpg',
   '/src/assets/images/image-product-4-thumbnail.jpg',
